f2cc74574cb709b2fb3b66da78dacaccb260a089
Your NameMilan <1978milan.babic@gmail.com>
2019-05-05 00:46:55 +0200

    signup confirm 1.0.2

--- routes/confacc.js
+++ routes/confacc.js
@@ -7,6 +7,14 @@ router.get('/:data', (req, res, next) => {
     //request params
     let reqData = req.params.data;
 
+    if (reqData.length > 0) {
+
+    } else {
+        const err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
+
     res.send(reqData);
 });
 
