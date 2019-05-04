5940327de17290a4a801a4ec955422d69df88e05
Your NameMilan <1978milan.babic@gmail.com>
2019-05-05 01:19:29 +0200

    path issue resolved

--- routes/confacc.js
+++ routes/confacc.js
@@ -3,19 +3,19 @@ const router = express.Router();
 //db model
 const User = require('../models/users');
 
+
 router.get('/:data', (req, res, next) => {
     //request params
     let reqData = req.params.data;
 
-    if (reqData.length > 0) {
-
+    if (reqData && reqData.length > 0) {
+        res.send(reqData);
     } else {
         const err = new Error('Not Found');
         err.status = 404;
         next(err);
     }
 
-    res.send(reqData);
 });
 
 module.exports = router;
